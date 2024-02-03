2b7363ea234eefbac4d8cb98c3ac27af4802d930
YASSINE00e <yassineazzouz51@gmail.com>
2024-02-03 11:52:58 +0100

    Updates
    Added Crosshair
    
    Added Light system (scripts/models)
    
    Added Pickup key system (scripts/models)
    
    Added Door system (scripts/models)

---
+++ Escape From School/Assets/Scripts/AdvancedDoors.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedDoors : MonoBehaviour
+{
+
+    [SerializeField] private Animator door;
+    [SerializeField] private GameObject keyOB;
+    [SerializeField] private GameObject openText;
+    //public GameObject closeText;
+    [SerializeField] private GameObject lockedText;
+
+
+    //[SerializeField] private AudioSource openSound;
+    //[SerializeField] private AudioSource lockedSound;
+    //[SerializeField] private AudioSource unlockedSound;
+
+    private bool inReach;
+    private bool doorisOpen;
+    private bool doorisClosed;
+    [SerializeField] private bool locked;
+    [SerializeField] private bool unlocked;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && doorisClosed)
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+            lockedText.SetActive(false);
+
+        }
+    }
+
+    void Start()
+    {
+        inReach = false;
+        doorisClosed = true;
+        doorisOpen = false;
+        openText.SetActive(false);
+        locked = true;
+        unlocked = false;
+    }
+
+
+
+
+    void Update()
+    {
+
+
+        if (inReach && keyOB.activeInHierarchy && Input.GetButtonDown("Interact"))
+        {
+            //unlockedSound.Play();
+            locked = false;
+            keyOB.SetActive(false);
+            StartCoroutine(unlockDoor());
+        }
+
+        if (inReach && doorisClosed && unlocked && Input.GetButtonDown("Interact"))
+        {
+            door.SetBool("Open", true);
+            door.SetBool("Closed", false);
+            openText.SetActive(false);
+            //openSound.Play();
+            doorisOpen = true;
+            doorisClosed = false;
+        }
+
+        if (inReach && locked && Input.GetButtonDown("Interact"))
+        {
+            openText.SetActive(false);
+            lockedText.SetActive(true);
+            //lockedSound.Play();
+        }
+
+    }
+
+    IEnumerator unlockDoor()
+    {
+        yield return new WaitForSeconds(.05f);
+        {
+
+            unlocked = true;
+        }
+    }
+}
---
+++ Escape From School/Assets/Scripts/PickUpKey.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    [SerializeField] private GameObject keyOB;
+    [SerializeField] private GameObject invOB;
+    [SerializeField] private GameObject pickUpText;
+    //[SerializeField] private AudioSource keySound;
+
+    [SerializeField] private bool inReach;
+
+
+    void Start()
+    {
+        inReach = false;
+        pickUpText.SetActive(false);
+        invOB.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            keyOB.SetActive(false);
+            //keySound.Play();
+            invOB.SetActive(true);
+            pickUpText.SetActive(false);
+        }
+
+
+    }
+}
