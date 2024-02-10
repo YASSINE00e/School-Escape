a1291682a0f8e3cc5af7bf1179be6643909f3182
YASSINE00e <yassineazzouz51@gmail.com>
2024-02-10 11:16:48 +0100

    Updates
    Added Read note system (Script/Image)
    add event system
    Edit some settings

---
+++ School Escape/Assets/Scripts/ReadNotes.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadNotes : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cam;
+    public GameObject noteUI;
+    public GameObject hud;
+    public GameObject inv;
+
+    public GameObject pickUpText;
+
+    //public AudioSource pickUpSound;
+
+    public bool inReach;
+
+
+
+    void Start()
+    {
+        noteUI.SetActive(false);
+        hud.SetActive(true);
+        inv.SetActive(true);
+        pickUpText.SetActive(false);
+
+        inReach = false;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+        }
+    }
+
+
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && inReach)
+        {
+            noteUI.SetActive(true);
+            //pickUpSound.Play();
+            hud.SetActive(false);
+            inv.SetActive(false);
+            player.GetComponent<PlayerMovement>().enabled = false;
+            cam.GetComponent<PlayerCam>().enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+
+    public void ExitButton()
+    {
+
+        noteUI.SetActive(false);
+        hud.SetActive(true);
+        inv.SetActive(true);
+        player.GetComponent<PlayerMovement>().enabled = true;
+        cam.GetComponent<PlayerCam>().enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+}
